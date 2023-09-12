39416f7e4848175754cedb7ea2778ca4ea4c136e
Lindsay Warr <iamalion@gmail.com>
2023-09-12 15:31:32 -0700

    WIP: MovieCard not importing properly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+
 import { useEffect } from 'react';
+import MovieCard from './MovieCard ';
+import './App.css';
+import SearchIcon from './search.svg';
+
 
 const API_URL = 'https://www.omdbapi.com?apikey=4c2ff4bd';
+
+const movie1 = 
+    {
+        "Title": "Murder by Death",
+        "Year": "1976",
+        "imdbID": "tt0074937",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BZWJiZjUxNTQtNTI3My00ZjQyLWFjZTEtODJjYzU0ZGYwYzNkXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg"
+    }
 
 const App = () => {
     const searchMovies = async (title) => {
@@ -15,9 +28,26 @@ const App = () => {
         searchMovies("Murder by Death")
     }, []);
     return (
-        <div>
-        <h1>React App</h1>
+        <div className="app">
+        <h1>MovieLand</h1>
+
+        <div className="search">
+            <input 
+                placeholder="Search for movies" 
+                value="Murder By Death" onChange={() => {}} 
+            />
+            <img
+                src = {SearchIcon}
+                alt = "search"
+                onClick = {() => {}}
+            />
+            </div>
+
+            <div className="containter">
+                <MovieCard movie1={movie1} /> 
+            </div>
         </div>
+       
     );
 }
 
